7d3ae4a31a362658d397452e5be93a2033da0d38
vindoh-3 <vincentdoh456@gmail.com>
2023-10-08 23:08:18 +0000

    Initial commit

--- resources/views/foods/cart.blade.php
+++ resources/views/foods/cart.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-xxl py-5 bg-dark hero-header mb-5"  style="margin-top: -25px">
+<div class="container-xxl py-5 bg-dark hero-header mb-5" style="margin-top: -25px">
     <div class="container text-center my-5 pt-5 pb-4">
         <h1 class="display-3 text-white mb-3 animated slideInDown">Cart</h1>
         <nav aria-label="breadcrumb">
